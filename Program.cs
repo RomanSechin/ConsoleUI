c7abcccb78a86d613704cabf2060b122ba26521f
romansechin <romansechin@gmail.com>
2024-10-02 15:39:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using StandardLibrary;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Generators generators = new Generators();
+        PersonModel person = new PersonModel
+        {
+            Prefix = "Mr.",
+            FirstName = "Tim",
+            LastName = "Corey",
+        };
+
+        string message = generators.WelcomeMessage(person.Prefix, person.LastName);
+        Console.WriteLine(message);
+        Console.ReadLine();
+    }
+}
